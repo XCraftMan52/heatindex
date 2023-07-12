8ec1b28cdb02c75bfa98862bfa3a0a6aac497022
Nathan Vick <nathanvick480@gmail.com>
2023-07-11 20:54:50 -0400

    Add Bootstrap

--- index.php
+++ index.php
@@ -21,33 +21,17 @@ $description = $color_data[1];
     <title>Heat Index</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
     <style>
         body {
             font-family: Inter, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
             margin: 0;
-        }
-
-        .btn {
-            background-color: #1c407d;
-            border: 2px solid #1c407d;
-            border-radius: 0.375rem;
-            box-shadow: none;
-            color: #ffffff;
-            cursor: pointer;
-            font-size: 1rem;
-            padding: 0.6rem 1rem;
-            transition: all 0.25s;
-        }
-
-        .btn:hover,
-        .btn:active {
-            background-color: #ffffff;
-            color: #1c407d;
         }
 
         #heat-index-content,
@@ -71,7 +55,12 @@ $description = $color_data[1];
 
         #heat-index-content header h1 {
             font-size: 2rem;
+            font-weight: bold;
             margin: 0;
+        }
+
+        #heat-index-content article {
+            margin: 0 1rem;
         }
 
         @media (min-width: 767px) {
@@ -107,14 +96,16 @@ $description = $color_data[1];
                     echo "<p>Location: " . $_GET["location"] . "</p>";
                 }
             ?>
-            <p id="flag-text">Flag Color: <?= ucfirst($color) ?></p>
         </header>
-        <p id="flag-description"><?= $description ?></p>
-        <p><?php
-            echo "Heat Index refreshed at: " . $temperature_timestamp . "<br>";
-            echo "Page refreshed at: " . $dt->setTimestamp($now)->format($datetime_format);
-        ?></p>
-        <button class="btn btn-refresh" onclick="window.location.reload()">Refresh</button>
+        <article>
+            <p id="flag-text">Flag Color: <?= ucfirst($color) ?></p>
+            <p id="flag-description"><?= $description ?></p>
+            <p><?php
+                echo "Heat Index refreshed at: " . $temperature_timestamp . "<br>";
+                echo "Page refreshed at: " . $dt->setTimestamp($now)->format($datetime_format);
+            ?></p>
+            <button class="btn btn-lg btn-primary mt-3" onclick="window.location.reload()">Refresh</button>
+        </article>
     </section>
     <section id="mandatory-refresh-content" style="display: none;">
         <p>Please <a href="" onclick="window.location.reload()">refresh</a> the page.</p>
